c251c10c3fe8f2341528cd7f9acb6e6738b2af83
Ghimire <sushan.ghimire@accenture.com>
2016-04-28 19:15:57 +0100

    Modified file path

--- selenium-ie-node-service.bat
+++ selenium-ie-node-service.bat
@@ -1,4 +1,4 @@
 rem ------- This script can be used as a service. 
-rem ------- eg: nssm install selenium-ie-service C:\Selenium\selenium-ie-node-service.bat
+rem ------- eg: nssm install selenium-ie-service C:\selenium-ie-node-service\selenium-ie-node-service.bat
 rem ------- To start the service: nssm start selenium-ie-service 
-java -jar C:\Selenium\selenium-server-standalone-2.53.0.jar -Dwebdriver.ie.driver=C:\Selenium\IEDriverServer.exe -role node -nodeConfig C:\Selenium\nodeconfig.json -port 5555 -hub http://172.16.68.238:4444/grid/register/
+java -jar C:\selenium-ie-node-service\selenium-server-standalone-2.53.0.jar -Dwebdriver.ie.driver=C:\selenium-ie-node-service\IEDriverServer.exe -role node -nodeConfig C:\selenium-ie-node-service\nodeconfig.json -port 5555 -hub http://172.16.68.238:4444/grid/register/
